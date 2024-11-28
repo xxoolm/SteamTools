8044cee366ad5be7a5080c5c9cd77ebe7ce2e91b
Aigio Liu <lysoft.pw@foxmail.com>
2024-11-28 14:44:36 +0800

    🐛 macos build

--- src/BD.WTTS.Client.Avalonia/UI/Views/Windows/MainWindow.axaml.cs
+++ src/BD.WTTS.Client.Avalonia/UI/Views/Windows/MainWindow.axaml.cs
@@ -251,8 +251,6 @@ public sealed class AppSplashScreen : IApplicationSplashScreen
                     App.Instance.CompositeDisposable.Add(IViewModelManager.Instance.MainWindow);
                     App.Instance.CompositeDisposable.Add(SteamConnectService.Current.Dispose);
 
-                    Helpers.TracepointHelper.TrackEvent("AppSplashScreen");
-
                     IsInitialized = true;
                 }
             }, cancellationToken: token);
